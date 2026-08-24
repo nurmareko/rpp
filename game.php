2f63d29eabe400326257b3bd775182e5529a8ca3
nurmareko <nurmareko@yandex.ru>
2026-08-25 02:14:02 +0700

    Add game choice form and logout handling

--- game.php
+++ game.php
@@ -4,8 +4,16 @@ print_r($_REQUEST);
 // DEBUG
 
 if (!isset($_GET['name'])) {
+    echo('name parameter missing');
     die();
 }
+
+if (isset($_POST['logout'])) {
+    exit(header('location: index.php'));
+}
+
+$name = $_GET['name'];
+$game_result = '';
 
 ?>
 
@@ -18,5 +26,19 @@ if (!isset($_GET['name'])) {
 </head>
 <body>
     <p>play it</p>
+    <p>welcome: <?= $name ?></p>
+    <form method="post">
+        <select name="human">
+            <option value="0">-- select --</option>
+            <option value="rock">rock</option>
+            <option value="paper">paper</option>
+            <option value="scissors">scissors</option>
+        </select>
+        <input type="submit" value="play">
+        <input type="submit" name="logout" value="logout">
+    </form>
+    <div style="background-color: #f5f5f5">
+        <p> <?= $game_result ?> </p>
+    </div>
 </body>
 </html>
